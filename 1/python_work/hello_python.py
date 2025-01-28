fe97efefae5acb8df8b9c39b178924df7a7c511e
Gnarlmor <grigore_gherman@icloud.com>
2025-01-28 23:50:30 +0200

    Variables, f-strings, Tabs & Newlines

---
+++ 1/python_work/hello_python.py
@@ -0,0 +1,5 @@
+message = "Hello Pyton World!"
+print(message)
+
+mesage = "Hello Python Crash Course world!"
+print(mesage)
---
+++ 1/python_work/simple_message.py
@@ -0,0 +1,5 @@
+student = "You will not pass the exams this semester"
+print(student)
+
+student = "Kidding you can pass everything, you can do it!"
+print(student)
