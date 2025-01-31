fbd1c7615aac29512f5075160df1ba6655fc53b7
Gnarlmor <grigore_gherman@icloud.com>
2025-01-31 22:12:35 +0200

    small change

---
+++ 1/python_work/Chapter 4/magicians.py
@@ -0,0 +1,9 @@
+#Learning how to use a for loop
+#Easy asociation: For every magician in the list of magicians, print the magician's name.
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(f"{magician.title()}, that was a great trick!")
+    print(f"I can't wait to see your next trick {magician.title()}\n")
+
+print("Thank you everyone. That was a great magic show!")
+
